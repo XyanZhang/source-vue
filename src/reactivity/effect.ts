92936a349655ccb863c0f8ef64075629e1ea1e60
XyanZhang <1743067175@qq.com>
2023-07-19 22:45:21 +0800

    feat: track trigger实现

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,10 +1,51 @@
+let activeEffect:any = null;
+// 用于依赖收集，当前ReactiveEffect
 class ReactiveEffect {
   private _fn: any;
   constructor(public fn) {
     this._fn = fn;
   }
   run() {
-    this._fn()
+    activeEffect = this; // 将当前的ReactiveEffect赋值给activeEffect
+    // 不是，每个effect都有一个ReactiveEffect实例
+    this._fn();
+  }
+}
+
+// 将容器放到WeakMap中, 为什么要用WeakMap，因为WeakMap的key是弱引用，不会造成内存泄漏
+const targetMap = new WeakMap();
+export function track(target, key) {
+  // 依赖收集
+  console.log('target: ', target, 'key: ', key);
+  // 构建一个容器
+  // target => key => dep
+  let depsMap = targetMap.get(target); // 取出target对应的容器
+  if (!depsMap) {
+    // 如果没有容器，就创建一个容器
+    depsMap = new Map()
+    targetMap.set(target, depsMap);
+  }
+  // 有容器，取出key对应的容器
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep); // 保存 key对应的容器
+  }
+  // set 操作
+  dep.add(activeEffect); // 将当前的ReactiveEffect存到容器中
+}
+
+export function trigger(target, key) {
+  // 触发依赖
+  const depsMap = targetMap.get(target);
+  if (!depsMap) {
+    return;
+  }
+  const dep = depsMap.get(key);
+  if (dep) {
+    dep.forEach(effect => {
+      effect.run();
+    })
   }
 }
 
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -17,7 +17,19 @@ describe("effect", () => {
     expect(nextAge).toBe(11);
     
     // update
-    user.age++;
+    user.age++; // 触发依赖 执行effect
+    // 注: 如何将 user.age 与 effect 中的 nextAge 关联起来 ? 
+    // 通过 track 和 trigger 来实现
+    // track 依赖收集: 将 effect 中的回调 存储到 targetMap 中
+    // trigger 触发依赖: 将 targetMap 中的回调 依次执行
+
+    // 多个 effect 时, 如何区分 ?
+    // 通过 activeEffect 来区分
+    // activeEffect: 当前正在执行的 effect
+    // 通过 activeEffect 来将 effect 中的回调fn 存储到 targetMap 中
+    // 通过 activeEffect 来将 targetMap 中的回调 依次执行
+
+    // 每个effect 函数都会new 一个 ReactiveEffect 实例, 所以 每个 effect 都有自己的 activeEffect
     expect(nextAge).toBe(12);
   });
 })

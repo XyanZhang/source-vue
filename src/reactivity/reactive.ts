22ecec2a616ade4e743e75dc306eecae5b532708
XyanZhang <1743067175@qq.com>
2023-07-27 20:34:47 +0800

    refactor: readonly reactive 重构

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -2,54 +2,52 @@ import { track, trigger } from "./effect";
 
 let isObject = (val) => typeof val === 'object' && val !== null;
 
-export function reactive(raw) {
-  return new Proxy(raw, {
-    get(target, key) {
-      if(key === '_is_reactive') return true;
-
-      let res =  Reflect.get(target, key);
-      if(isObject(res)) {
-        return reactive(res);
-      }
-      // 依赖收集
-      track(target, key); // target: {age: 10}, key: age
-
-      return res;
-    },
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value);
-      // 触发依赖
-      trigger(target, key); // target: {age: 10}, key: age
-
-      return res;
+function createGetter(isReadonly) {
+  return function get(target, key, receiver) {
+    const res = Reflect.get(target, key, receiver);
+    if(key === '_is_reactive') {
+      return !isReadonly;
     }
+    if(key === '_is_readonly') {
+      return isReadonly;
+    }
+    // 依赖收集
+    track(target, key); // target: {age: 10}, key: age
+
+    if(isObject(res)) {
+      return isReadonly ? readonly(res) : reactive(res);
+    }
+
+    return res;
+  }
+}
+
+function createSetter(isReadonly) {
+  return function setter(target, key, value) {
+    const res = Reflect.set(target, key, value);
+    if(isReadonly) {
+      console.warn(`set ${JSON.stringify(target)} on key ${key} failed`);
+      return true;
+    }
+    // 触发依赖
+    trigger(target, key); // target: {age: 10}, key: age
+    return res;
+  }
+}
+
+export function reactive(raw) {
+  return createReactiveObject(raw)
+}
+
+export function createReactiveObject(raw, isReadonly?) {
+  return new Proxy(raw, {
+    get: createGetter(isReadonly),
+    set: createSetter(isReadonly)
   })
 }
 
 export function readonly(raw) {
-  console.log('raw', raw);
-  return createReadonlyObject(raw);
-}
-
-function createReadonlyObject(raw) {
-  let proxy = new Proxy(raw, {
-    get(target, key, receiver) {
-      console.log('receiver', receiver)
-      if(key === '_is_readonly') return true;
-      
-      let res =  Reflect.get(target, key, receiver);
-      if(isObject(res)) {
-        return readonly(res);
-      }
-      return res;
-    },
-    set(target, key:any, value) {
-      console.warn(`key: ${key} set failed`);
-      return true;
-    }
-  })
-  console.log(proxy)
-  return proxy;
+  return createReactiveObject(raw, true);
 }
 
 export function isReactive(value) {

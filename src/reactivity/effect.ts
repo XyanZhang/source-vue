ccebdbcc8bc6b81cd790b498fa0230d46bf6e948
XyanZhang <1743067175@qq.com>
2023-07-31 20:55:46 +0800

    refactor: effect

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,4 +1,6 @@
 let activeEffect:any = null;
+
+let shouldTrack = false;
 // 用于依赖收集，当前ReactiveEffect
 export class ReactiveEffect {
   active = true; // 默认是激活的
@@ -8,21 +10,55 @@ export class ReactiveEffect {
     this._fn = fn;
   }
   run() {
-    activeEffect = this; // 将当前的ReactiveEffect赋值给activeEffect
-    // 不是，每个effect都有一个ReactiveEffect实例
-    return this._fn();
+    console.log("run");
+    // 运行 run 的时候，可以控制 要不要执行后续收集依赖的一步
+    // 目前来看的话，只要执行了 fn 那么就默认执行了收集依赖
+    // 这里就需要控制了
+
+    // 是不是收集依赖的变量
+
+    // 执行 fn  但是不收集依赖
+    if (!this.active) {
+      return this.fn();
+    }
+
+    // 执行 fn  收集依赖
+    // 可以开始收集依赖了
+    shouldTrack = true;
+
+    // 执行的时候给全局的 activeEffect 赋值
+    // 利用全局属性来获取当前的 effect
+    activeEffect = this as any;
+    // 执行用户传入的 fn
+    console.log("执行用户传入的 fn");
+    const result = this.fn();
+    // 重置
+    shouldTrack = false;
+    activeEffect = undefined;
+
+    return result;
   }
   stop() {
     if(this.active) {
-      
-      this.deps.forEach((dep: any) => {
-        dep.delete(this);
-      });
-      this.onStop && this.onStop();
-
+      // 如果第一次执行 stop 后 active 就 false 了
+      // 这是为了防止重复的调用，执行 stop 逻辑
+      cleanupEffect(this);
+      if (this.onStop) {
+        this.onStop();
+      }
       this.active = false;
     }
   }
+}
+
+function cleanupEffect(effect) {
+  // 找到所有依赖这个 effect 的响应式对象
+  // 从这些响应式对象里面把 effect 给删除掉
+  effect.deps.forEach((dep) => {
+    dep.delete(effect);
+  });
+
+  effect.deps.length = 0;
 }
 
 export function stop(runner) {
@@ -103,4 +139,8 @@ export function effect(fn, options: any = {}) {
   runner.effect = _effect;
 
   return runner;
+}
+
+export function isTracking() {
+  return shouldTrack && activeEffect !== undefined;
 }
